07fc5f3937b6b85392302cd54e3a2d26d4b72dfa
Radu Camerzan <raduttzu@gmail.com>
2017-06-22 20:44:58 +0300

    WIP: Vue first try.

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const expressVue = require('express-vue');
+const app = express();
+app.set('views', __dirname + '/vue/views');
+//Optional if you want to specify the components directory separate to your views, and/or specify a custom layout. 
+app.set('vue', {
+    //ComponentsDir is optional if you are storing your components in a different directory than your views 
+    componentsDir: __dirname + '/vue/components',
+    //Default layout is optional it's a file and relative to the views path, it does not require a .vue extension. 
+    //If you want a custom layout set this to the location of your layout.vue file. 
+    defaultLayout: 'layout'
+});
+app.engine('vue', expressVue);
+app.set('view engine', 'vue');
+
+app.get('/', (req, res) => {
+  res.render('main', {
+    data: {
+      otherData: 'Something Else'
+    },
+    vue: {
+      head: {
+        title: 'Show all users',
+        head: [
+          { property:'og:title', content: 'Page Title'},
+          { name:'twitter:title', content: 'Page Title'},
+      ]
+      }    
+    }
+  });
+})
 
 // Vue.js pages will be routed here.
 app.get('/vue', function (req, res) {
