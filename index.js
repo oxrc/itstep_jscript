52ea12b181001ab97421a0887f010a4ea5cda82f
MentosMD <mentos.md@mail.ru>
2017-06-23 10:02:49 +0400

    Render basic vue.

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 const express = require('express')
 const expressVue = require('express-vue');
 const app = express();
-app.set('views', __dirname + '/vue/views');
-//Optional if you want to specify the components directory separate to your views, and/or specify a custom layout. 
+
+
+app.engine('vue', expressVue);
+app.set('view engine', 'vue');
+app.set('views', path.join(__dirname, '/views'));
 app.set('vue', {
-    //ComponentsDir is optional if you are storing your components in a different directory than your views 
-    componentsDir: __dirname + '/vue/components',
-    //Default layout is optional it's a file and relative to the views path, it does not require a .vue extension. 
-    //If you want a custom layout set this to the location of your layout.vue file. 
+    componentsDir: path.join(__dirname, '/vue/components'),
     defaultLayout: 'layout'
 });
+
+
 app.engine('vue', expressVue);
 app.set('view engine', 'vue');
 
-app.get('/', (req, res) => {
-  res.render('main', {
-    data: {
-      otherData: 'Something Else'
-    },
-    vue: {
-      head: {
-        title: 'Show all users',
-        head: [
-          { property:'og:title', content: 'Page Title'},
-          { name:'twitter:title', content: 'Page Title'},
-      ]
-      }    
-    }
+app.get('/', (req, res)  {
+  var scope = {
+        data: {
+            title: pageTitle,
+            message: 'Hello!'
+        },
+        vue: {
+            head: {
+                title: pageTitle,
+                meta: [
+                    { property:'og:title', content: pageTitle},
+                    { name:'twitter:title', content: pageTitle}
+                 ]
+                }
+            },
+            mixins: [exampleMixin]
+        }
+    };
+    res.render('main', scope);
   });
 })
+
+
+
 
 // Vue.js pages will be routed here.
 app.get('/vue', function (req, res) {
--- vue/views/main.vue
+++ vue/views/main.vue
@@ -1,5 +1,9 @@
-<template>
+<template lang="html">
     <div>
-        <h1>{{ otherData }}</h1>
+        <h1>{{title}}</h1>
+        <p>Welcome to the {{title}} demo. Click a link:</p>
+        <button type="button" name="button">Say FOO</button>
+        <input v-model="message" placeholder="edit me">
     </div>
 </template>
+
