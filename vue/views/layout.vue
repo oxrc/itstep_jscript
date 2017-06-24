6bd761561c662a80ab1ca9c8db6b101c51776286
Mentos <mentos.md@mail.ru>
2017-06-24 16:32:44 +0300

    Main_PageLayout

--- vue/views/layout.vue
+++ vue/views/layout.vue
@@ -5,12 +5,62 @@
             <meta charset="utf-8">
              <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" >
             <link rel="stylesheet" type="text/css" href="/css/style.css" >    
         </head>
         <body>
              <div class="row page-wrapper">
-                 <div class="col-lg-6" style="padding:0;"></div>
-                 <div class="col-lg-6" style="padding:0;"></div>
+                 <div class="col-lg-5 searc_user" style="padding:0;">
+                     
+                 </div>
+                 <div class="col-lg-7" id="tables_users">
+                     <table class="table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Age</th>
+                                <th>Telephone</th>
+                                <th>Interest</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Joke</td>
+                                <td>23</td>
+                                <td>757834965</td>
+                                <td>Sport</td>
+                                <td><a href="#"  id="btn_delete">Удалить</a></td>
+                                <td><a href="#"  id="btn_edit">Редактировать</a></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Joke</td>
+                                <td>23</td>
+                                <td>757834965</td>
+                                <td>Sport</td>
+                                <td><a href="#"  id="btn_delete">Удалить</a></td>
+                                <td><a href="#"  id="btn_edit">Редактировать</a></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Joke</td>
+                                <td>23</td>
+                                <td>757834965</td>
+                                <td>Sport</td>
+                                <td><a href="#"  id="btn_delete">Удалить</a></td>
+                                <td><a href="#"  id="btn_edit">Редактировать</a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                 </div>
               </div>
          
          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
