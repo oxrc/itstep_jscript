500c9a1e7481b23a0f9a48a437d2f64c92069e6b
vberco <vberco.76@gmail.com>
2017-06-27 20:02:02 +0300

    #home did in class

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,24 @@
-function start() {
+var exec = require("child_process").exec;
+var sqlite3 = require('sqlite3').verbose();
+var file = "./usersdb";
+console.log(file);
+var db = new sqlite3.Database(file);
+
+function start(response) {
     console.log("Request handler 'start' was called.");
-    return 'Hello from start';
+    db.each("SELECT * from users u", function(err, row) {
+        console.log(row.id, row.name, row.age, row.phone)
+    });
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write("rows");
+    response.end();
 }
 
-function upload() {
+function upload(response) {
     console.log("Request handler 'upload' was called.");
-    return 'Hello from upload';
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write("Hello Upload");
+    response.end();
 }
 
 exports.start = start;
