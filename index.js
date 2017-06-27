880bd923eacc81f4920c829a3ddfd0b68a74249f
Radu Camerzan <raduttzu@gmail.com>
2017-06-27 19:38:46 +0300

    Code formatting.

--- index.js
+++ index.js
@@ -3,45 +3,41 @@ const express = require('express')
 const expressVue = require('express-vue');
 const app = express();
 
-
-app.engine('vue', expressVue)
-app.set('view engine', 'vue')
-app.set('views', path.join(__dirname, '/vue/views'))
+// Init vue app.
+app.engine('vue', expressVue);
+app.set('view engine', 'vue');
+app.set('views', path.join(__dirname, '/vue/views'));
 app.set('vue', {
-    componentsDir: path.join(__dirname, '/vue/components'),
-    defaultLayout: 'layout'
-})
-app.use(express.static(path.join(__dirname, 'public')))
-
-
-
+  componentsDir: path.join(__dirname, '/vue/components'),
+  defaultLayout: 'layout'
+});
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Main page
-app.get('/vue', function(req, res){
-    res.render('main')
+app.get('/vue', function(req, res) {
+  res.render('main');
 });
 
 //Add user
-app.get('/vue/adduser', function(req, res){
-    var scope = {
-        data: {
-            title: 'Add user'
-        }
+app.get('/vue/adduser', function(req, res) {
+  var scope = {
+    data: {
+      title: 'Add user'
     }
-    res.render('add_user', scope)
+  }
+  res.render('add_user', scope);
 });
 
 //Add interes
-app.get('/vue/addinteres', function(req, res){
-     res.render('add_interes')
+app.get('/vue/addinteres', function(req, res) {
+  res.render('add_interes');
 });
 
 app.get('/vue/edit/:id', function(req, res){
-     res.send(req.params);
+  res.send(req.params);
 })
 
-
 // Making the app listen to port 80 for requests.
-app.listen(80, function () {
-  console.log('App is listening on the 80 port.')
+app.listen(81, function() {
+  console.log('App is listening on the 80 port.');
 })
--- vue/views/add_user.vue
+++ vue/views/add_user.vue
@@ -1,26 +1,26 @@
 <template lang="html">
-    <div id="add_user_content" class="row">
-        <div class="container" style="text-align:center;">
-           <form action="" method="post">
-                          <div class="form-group">
-                                <input type="text" placeholder="First Name" />
-                          </div>
-                          <div class="form-group">
-                                <input type="text" placeholder="Last Name" />
-                          </div>
-                          <div class="form-group">
-                                <input type="text" placeholder="Age" />
-                          </div>
-                          <div class="form-group">
-                                <input type="text" placeholder="Telephone" />
-                          </div>
-                          <div class="form-group">
-                                <input type="text" placeholder="Interest" />
-                          </div>
-                          <button type="submit" class="btn_search"><span class="fa fa-user-plus"></span> Add user</button>
-             </form>
-        </div>
-    </div>
+<div id="add_user_content" class="row">
+      <div class="container" style="text-align:center;">
+            <form action="" method="post">
+                  <div class="form-group">
+                        <input type="text" placeholder="First Name" />
+                  </div>
+                  <div class="form-group">
+                        <input type="text" placeholder="Last Name" />
+                  </div>
+                  <div class="form-group">
+                        <input type="text" placeholder="Age" />
+                  </div>
+                  <div class="form-group">
+                        <input type="text" placeholder="Telephone" />
+                  </div>
+                  <div class="form-group">
+                        <input type="text" placeholder="Interest" />
+                  </div>
+                  <button type="submit" class="btn_search"><span class="fa fa-user-plus"></span>Add user</button>
+            </form>
+      </div>
+</div>
 </template>
 
 
