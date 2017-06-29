a03a124698461e3abd278e3271431e15bf034a86
Mentos <mentos.md@mail.ru>
2017-06-29 20:01:18 +0300

    Added skeleton!

--- vue/views/layout.vue
+++ vue/views/layout.vue
@@ -4,7 +4,7 @@
         <title>Main layout</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css" >
+        <link rel="stylesheet" type="text/css" href="/css/skeleton.css" >
         <link rel="stylesheet" type="text/css" href="/css/style.css" >
         <script src="/js/vue.js"></script>
         <script src="/js/axios.js"></script>
