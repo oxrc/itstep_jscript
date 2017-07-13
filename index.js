56a67f8d350e284b1b8e14213fb655bacf73f739
Radu Camerzan <raduttzu@gmail.com>
2017-07-13 19:41:35 +0300

    Code formatting and removing obsolete code.

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ app.set('vue', {
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Init default variables
-var pageTitle='Main page';
+var pageTitle = 'Main page';
 
 // Main page
-app.get('/vue', function(req, res) {
+app.get('/vue', function (req, res) {
   res.render('main');
 });
 
@@ -47,7 +47,7 @@ app.get('/vue/user/add', function (req, res) {
 });
 
 //Add interes
-app.get('/vue/addinteres', function(req, res) {
+app.get('/vue/addinteres', function (req, res) {
   res.render('add_interes');
 });
 
@@ -55,70 +55,20 @@ app.get('/vue/user/edit/:id', function (req, res) {
   res.send(req.params);
 });
 
-// Server mockups
-var user_list = {
-  users: [
-    {
-      id: 1,
-      name: "Valera",
-      age: 18,
-      phone: "+37379865764",
-      interests: [1, 5, 7, 8]
-    },
-    {
-      id: 2,
-      name: "Maria",
-      age: 20,
-      phone: "+37379003413",
-      interests: [2, 7, 8]
-    },
-    {
-      id: 3,
-      name: "Ionela",
-      age: 25,
-      phone: "+37369903436",
-      interests: [4, 5]
-    },
-    {
-      id: 4,
-      name: "Ionela",
-      age: 21,
-      phone: "+37369654688",
-      interests: [1, 2, 3]
-    },
-  ]
-}
-app.get('/api/user/list', function (req, res) {
-  res.json(user_list);
-});
-app.get('/api/user/:uid', function (req, res) {
-  let uid = parseInt(req.params.uid, 10);
-  let existing_user;
-  for (let user of user_list.users) {
-    if (uid == user.id) {
-      existing_user = user;
-    }
-  }
-  if (undefined === existing_user) {
-    existing_user = { error: 'User not found' };
-  }
-  res.json(existing_user);
-});
-
 // Server endpoints.
 app.get('/api/interests', function (req, res) {
   serverHandlers.interests(res);
 });
 
-app.get('/api/users', function(req, res){
+app.get('/api/users', function (req, res) {
   serverHandlers.getUsers(res, req);
 });
 
-app.get('/api/interests/add', function(req, res){
+app.get('/api/interests/add', function (req, res) {
   serverHandlers.getUsers(res, req);
 });
 
 // Making the app listen to port 80 for requests.
-app.listen(80, function() {
+app.listen(80, function () {
   console.log('App is listening on the 80 port.');
 });
--- requestHandlers.js
+++ requestHandlers.js
@@ -7,80 +7,77 @@ var queryString = require('querystring');
 var db = new sqlite3.Database(file);
 
 function start(response) {
-    console.log("Request handler 'start' was called.");
-    db.each("SELECT * from users u", function(err, row) {
-        console.log(row.id, row.name, row.age, row.phone)
-    });
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("rows");
-    response.end();
+  console.log("Request handler 'start' was called.");
+  db.each("SELECT * from users u", function (err, row) {
+    console.log(row.id, row.name, row.age, row.phone)
+  });
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.write("rows");
+  response.end();
 }
 
 // Get the list of all users.
 function getUsers(response, request) {
-    var users = [];
-    var query = url.parse(request.url).query;
-    var object = queryString.parse(query);
-    var offset;
-    if (object['page'] == null || object['page'] == 1) {
-        offset = 0;
-    } else {
-        offset = object['page'];
+  var users = [];
+  var query = url.parse(request.url).query;
+  var object = queryString.parse(query);
+  var offset;
+  if (object['page'] == null || object['page'] == 1) {
+    offset = 0;
+  }
+  else {
+    offset = object['page'];
+  }
+  var dbQuery = "SELECT u.id, u.name, u.age, u.phone, GROUP_CONCAT(i.int_id, ',') AS interests FROM users u, interests i WHERE u.id = i.uid GROUP BY u.id LIMIT 5 OFFSET " + offset;
+  db.all(dbQuery, function (err, rows) {
+    for (row in rows) {
+      userData = {};
+      userData['id'] = rows[row].id;
+      userData['name'] = rows[row].name;
+      userData['age'] = rows[row].age;
+      userData['phone'] = rows[row].phone;
+      userData['interests'] = rows[row].interests;
+      users.push(userData);
     }
-    var dbQuery = "SELECT u.id, u.name, u.age, u.phone, GROUP_CONCAT(i.int_id, ',') AS interests FROM users u, interests i WHERE u.id = i.uid GROUP BY u.id LIMIT 5 OFFSET " + offset;
-    db.all(dbQuery, function(err, rows) {
-        for (row in rows) {
-            userData = {};
-            userData['id'] = rows[row].id;
-            userData['name'] = rows[row].name;
-            userData['age'] = rows[row].age;
-            userData['phone'] = rows[row].phone;
-            userData['interests'] = rows[row].interests;
-            users.push(userData);
-        }
 
-        response.writeHead(200, { "Content-Type": "application/json" });
-        response.write(JSON.stringify(users));
-        response.end();
-    });
+    response.json(users);
+  });
 }
 
 // Get the list of all interests.
 function interests(response) {
-    console.log("Request handler 'interests' was called.");
-    var interestsList = {};
+  console.log("Request handler 'interests' was called.");
+  var interestsList = {};
 
-    db.all("SELECT i_id, i_name from interests_list", function(err, rows) {
-        for (row in rows) {
-            interestsList[rows[row].i_id] = rows[row].i_name;
-        }
-        response.json(interestsList);
-    });
+  db.all("SELECT i_id, i_name from interests_list", function (err, rows) {
+    for (row in rows) {
+      interestsList[rows[row].i_id] = rows[row].i_name;
+    }
+    response.json(interestsList);
+  });
 }
 
 //Add interes.
 function addInterests(response, request) {
-    var query = url.parse(request.url).query;
-    var queryObject = queryString.parse(query);
-   
-    var dbQuery = "INSERT INTO interests_list (i_name) VALUES (" + "'" + queryObject['interest'] + "'" + ")";
-     console.log(dbQuery);
-    var stmt = db.prepare(dbQuery);
-    console.log();
-    stmt.run();  
-    stmt.finalize();  
-    console.log("Request handler 'addInterest' was called.");
-    
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("Add interest");
-    response.end();
+  var query = url.parse(request.url).query;
+  var queryObject = queryString.parse(query);
+
+  var dbQuery = "INSERT INTO interests_list (i_name) VALUES (" + "'" + queryObject['interest'] + "'" + ")";
+  var stmt = db.prepare(dbQuery);
+  stmt.run();
+  stmt.finalize();
+  console.log("Request handler 'addInterest' was called.");
+
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.write("Add interest");
+  response.end();
 }
 
 function upload(response) {
-    console.log("Request handler 'upload' was called.");
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("Hello Upload");
-    response.end();
+  console.log("Request handler 'upload' was called.");
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.write("Hello Upload");
+  response.end();
 }
 
 exports.start = start;
