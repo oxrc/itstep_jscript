5466d4966ad1e747dd273369f2031af403ab3baf
campus <campus@iic.md>
2017-07-13 19:20:08 +0300

    feature add funcional adding user

--- src/app/user_add.component.ts
+++ src/app/user_add.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Http, Response, HttpModule } from '@angular/http';
-import 'rxjs/Rx';
+import {Injectable} from '@angular/core';
+//import {Http, Response} from '@angular/http';
+//import {Observable} from 'rxjs/Rx';
+
 
 
 @Component({
@@ -15,17 +18,17 @@ export class User_addComponent implements OnInit {
 			echo "<option value='{$interests[$key]['id']}'>{$interests[$key]['interes']}</option>";
 	?>*/
 
-//	public interests;
-//    constructor(http: Http) {
-//        http.get('http://127.0.0.1/api/get_interests').map((res: Response) => res.json())
-//		.subscribe(res => this.interests = res.interests);
-//    }	
+	public interests;
+    constructor(http: Http) {
+        http.get('http://127.0.0.1/api/get_interests').map((res: Response) => res.json())
+		.subscribe(res => this.interests = res.interests);
+    }	
 	
-    public interests = [
-		{ interes: 'Jilles', id: 21 },
-		{ interes: 'Todd', id: 24 },
-		{ interes: 'Lisa', id: 18 }
-    ];
+//    public interests = [
+//		{ interes: 'Something', id: 21 },
+//		{ interes: 'Another something', id: 24 },
+//		{ interes: 'The anotherest something', id: 18 }
+//    ];
   ngOnInit(): void {
   }
 }
