e6d7774a8048ae1cdc263f18c16d8a66a09d09b7
MentosMD <ciocoi.cristin@gmail.com>
2017-07-20 20:18:03 +0300

    Changed get_users and position elements

--- src/app/user_edit.component.ts
+++ src/app/user_edit.component.ts
@@ -3,27 +3,22 @@ import { Component, OnInit }        from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Location }                 from '@angular/common';
 
-import { Hero }         from './hero';
-import { HeroService }  from './hero.service';
+
 @Component({
   selector: 'user_edit',
   templateUrl: './user_edit.component.html',
   styleUrls: [ './user_edit.component.css' ]
 })
-export class User_editComponent implements OnInit {
-  hero: Hero;
+export class User_editComponent  {
+
 
   constructor(
-    private heroService: HeroService,
+    
     private route: ActivatedRoute,
     private location: Location
   ) {}
 
-  ngOnInit(): void {
-    this.route.paramMap
-      .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
-      .subscribe(hero => this.hero = hero);
-  }
+ 
 
   goBack(): void {
     this.location.back();
