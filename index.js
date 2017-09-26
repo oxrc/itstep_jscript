19cce1c8be11147955cc936f6b9c6d928e67c32a
vberco <vberco.76@gmail.com>
2017-09-26 20:39:51 +0300

    #login

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ handle["/api/users/edit"] = requestHandlers.editUser;
 handle["/api/users/delete"] = requestHandlers.deleteUser;
 handle["/api/users/search"] = requestHandlers.getUsersByParameters;
 handle["/api/users/get"] = requestHandlers.getUsersById;
-handle['api/users/login'] = login.loginUser;
+handle["/api/users/login"] = login.loginUser;
+handle["/api/users/logout"] = login.logoutUser;
 
 server.start(router.route, handle);
 
--- login.js
+++ login.js
@@ -4,7 +4,7 @@ var file = "./usersdb";
 var url = require('url');
 var queryString = require('querystring');
 var bcrypt = require('bcrypt');
-// var db = new sqlite3.Database(file);
+var db = new sqlite3.Database(file);
 var users = [
     { login: 'Anton', password: 'step1' },
     { login: 'Andrei', password: 'step2' },
@@ -15,44 +15,11 @@ var users = [
     { login: 'Radu', password: 'step0' }
 ];
 
-// function getUsersHashPassword(users) {
-//     for (var i = 0; i < users.length; i++) {
-//         let hash = bcrypt.hash(users[i].password, 10);
-//         users[i].password = hash;
-//     }
-//     return users;
-// }
+function logoutUser(response, request) {
 
-function getConnectionObject() {
-    var db = new sqlite3.Database(file);
-    db.get(
-        "SELECT name FROM sqlite_master WHERE type='table' AND name='login_data'",
-        function(err, row) {
-            if (!row.name) {
-                db.run("CREATE TABLE login_data (uid integer not null, user_login varchar not null, password varchar not null)",
-                    function(err, res) {
-                        if (err) {
-                            return console.error(err.message);
-                        }
-                        for (var i = 0; i < users.length; i++) {
-                            let hash = bcrypt.hash(users[i].password, 10);
-                            var query = "INSERT INTO login_data (user_login, password) VALUES (" + users[i].login + "," + hash + ")";
-                            db.run(query, function(err) {
-                                if (err) {
-                                    return console.error(err.message);
-                                }
-                            });
-                        }
-                        return db;
-                    });
-            } else {
-                return db;
-            }
-        });
 }
 
 function loginUser(response, request) {
-    var db = getConnectionObject();
     var data;
     if (request.method == "POST") {
         var body = '';
@@ -66,17 +33,25 @@ function loginUser(response, request) {
         var query = url.parse(request.url).query;
         data = queryString.parse(query);
     }
-    var query = "SELECT uid, user_login, password FROM login_data WHERE login=" + data.login;
+
+    var query = "SELECT * FROM login_data WHERE login=" + '"' + data.login + '"';
     db.get(query, function(err, res) {
-        var responce;
+        var responce = {};
         if (err) {
-            var message = "Wrong login";
-            return console.error(message);
+
+            return responce.login = "false";
         }
-        if (data.password != res.password || bcrypt.compareSync(data.password, res.password)) {
-            responce = false;
+        if (data.password === res.password || bcrypt.compareSync(data.password, res.password)) {
+            var query = "UPDATE login_data SET status = 0 WHERE uid =" + res.uid;
+            db.run(query, function(err, res) {
+                if (err) {
+                    return console.error(err.message);
+                }
+                return responce.autorization = "true";
+
+            });
         } else {
-            responce = true;
+            return responce.password = "false";
         }
         response.writeHead(200, { "Content-Type": "application/json" });
         response.write(JSON.stringify(responce));
@@ -84,5 +59,7 @@ function loginUser(response, request) {
     });
 
 }
+
+
 
 exports.loginUser = loginUser;
